65c25f2ed46d4e7fe4187d8e5487f9c5d85312e6
Footandhill <1477749479@qq.com>
2023-04-06 00:58:18 +0800

    day_one

---
+++ java基础/代码/java/src/com/work/base/javaComment.java
@@ -0,0 +1,17 @@
+package com.work.base;
+
+/**
+ * @author 这是一个编写Java注释的复习
+ */
+public class javaComment {
+    public static void main(String[] args) {
+        //    当行注释
+        System.out.println("单行注释");
+        /*
+        *多行注释
+        *可以注释多行内容
+        */
+        System.out.println("多行注释");
+
+    }
+}
