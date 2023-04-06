f111f1e4984ff3bedf8dfcd95af22cfaef1b04ce
Footandhill <1477749479@qq.com>
2023-04-07 00:22:08 +0800

    day_one

---
+++ java基础/代码/java/src/com/work/base/javaConstants.java
@@ -0,0 +1,23 @@
+package com.work.base;
+
+/**
+ * @author 常量的创建和使用
+ *
+ */
+public class javaConstants {
+//    静态常量
+    public static final double PI = 3.14;
+
+//    声明成员常量
+    final int X = 3;
+
+    public static void main(String[] args) {
+//        声明局部常量
+        final String Y = "asa";
+
+        System.out.println(PI);
+//        System.out.println(X);          无法从static中引入非static字段
+        System.out.println(Y);
+    }
+
+}
---
+++ java基础/代码/java/src/com/work/base/javaVariables.java
@@ -0,0 +1,7 @@
+package com.work.base;
+
+public class javaVariables {
+    public static void main(String[] args) {
+
+    }
+}
